1844984f116b793c04a05fb46e222526c83e95a8
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-04-15 17:36:27 -0300

    Create operadores_de_atribuicao.py

---
+++ operadores_de_atribuicao.py
@@ -0,0 +1,36 @@
+#ATRIBUIÇÃO SIMPLES
+saldo = 500
+print(saldo)
+
+#ATRIBUIÇÃO COM ADIÇÃO
+saldo += 200 #saldo = saldo + 200
+print(saldo)
+
+#ATRIBUIÇÃO COM SUBTRAÇÃO
+saldo = 500
+saldo -= 100
+print(saldo) #saldo = saldo - 100
+
+#ATRIBUIÇÃO COM MULTIPLICAÇÃO
+saldo = 500
+saldo *= 2
+print(saldo) #saldo = saldo * 2
+
+#ATRIBUIÇÃO COM DIVISÃO
+saldo = 500
+saldo /= 5
+print(saldo) #saldo = saldo / 2
+
+saldo = 500
+saldo //= 5
+print(saldo) #saldo = saldo // 5
+
+#ATRIBUIÇÃO COM MÓDULO
+saldo = 500
+saldo %= 480
+print(saldo) #saldo = saldo % 480
+
+#ATRIBUIÇÃO COM EXPONENCIAÇÃO
+saldo = 500
+saldo **= 2
+print(saldo) #saldo = saldo ** 2
