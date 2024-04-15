c8e688b0109333fda0ad49986c9f394f786fceb9
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-04-15 17:20:33 -0300

    Create operadores_de_comparacao.py

---
+++ operadores_de_comparacao.py
@@ -0,0 +1,21 @@
+saldo = 450
+saque = 250
+
+#IGUALDADE
+print(saldo == saque) #vai retornar "false"
+
+#DIFERENÇA
+print(saldo != saque) #vai retornar "true"
+
+#MAIOR QUE / MAIOR OU IGUAL
+print(saldo > saque) #vai retornar "true"
+print(saldo >= saque) #vai retornar "true"
+
+#MENOR QUE / MENOR OU IGUAL
+print(saldo < saque) #vai retornar "false"
+print(saldo <= saque) #vai retornar "false"
+
+
+
+
+
