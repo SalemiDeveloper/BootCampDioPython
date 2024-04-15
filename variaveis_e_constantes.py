bc0122c5df05e27e6f0272b14f78e7f113bce537
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-04-15 17:10:23 -0300

    Create variaveis_e_constantes.py

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,16 @@
+nome = 'Pedro'
+idade = 22
+
+print(nome, idade)
+
+nome = 'Marina'
+
+print(nome)
+
+limite_saque_diario = 1000
+
+print(limite_saque_diario)
+
+STATES = ["SP", "RJ", "MG"]
+
+print(STATES)
