1e8abdb57ae09952d61b29305362aa95ff805cdd
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-04-16 15:36:06 -0300

    Create operadores_de_associoacao.py

---
+++ operadores_de_associoacao.py
@@ -0,0 +1,7 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+print("Python" in curso) #True
+print("maçã" not in frutas) #False
+print(200 in saques) #False
