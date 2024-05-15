515895ba89ae074a8360010987e8ba5fcbc79526
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 17:03:19 -0300

    Create dicionarios_aninhados.py

---
+++ dicionarios_aninhados.py
@@ -0,0 +1,8 @@
+contatos = {
+    "pedro@gmail.com": {"nome" : "Pedro", "telefone": "1111-1111"}, 
+    "joao@gmail.com": {"nome": "João", "telefone": "2222-2222"}, 
+    "gustavo@gmail.com": {"nome": "Gustavo", "telefone": "3333-3333"}
+}
+
+telefone = contatos["joao@gmail.com"]["telefone"]
+print(telefone)
