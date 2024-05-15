a95f2d79c69208483e9a0f96188f9a50250ba117
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 17:02:58 -0300

    Create dicionarios.py

---
+++ dicionarios.py
@@ -0,0 +1,13 @@
+pessoa = {"nome" : "Pedro", "idade" : 22}
+print(pessoa)
+
+pessoa = dict(nome="Pedro", idade=22)
+print(pessoa)
+
+pessoa["telefone"] = "22680393"
+print(pessoa)
+
+#acessando dados do dicionário
+print(pessoa["nome"])
+print(pessoa["idade"])
+print(pessoa["telefone"])
