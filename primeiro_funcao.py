e119c7ac8989a91614728f6e7d15a121f16e28e5
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-16 00:55:15 -0300

    Create primeiro_funcao.py

---
+++ primeiro_funcao.py
@@ -0,0 +1,16 @@
+def exibir_mensagem():
+    print("Olá Mundo!")
+
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Pedro")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="João")
