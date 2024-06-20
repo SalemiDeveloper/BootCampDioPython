9765f782ed18f8d1db82d623b06fd0987c8496f1
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-06-20 19:48:29 -0300

    Create primary_key.sql

---
+++ SQL/primary_key.sql
@@ -0,0 +1,15 @@
+--PRATICANDO PRIMARY KEY
+-- Tabela "usuarios"
+ALTER TABLE usuarios
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "destinos"
+ALTER TABLE destinos
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "reservas"
+ALTER TABLE reservas
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
